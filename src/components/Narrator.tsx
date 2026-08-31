5db386aaeb79c781d2486c29d7f62fb8553dd93e
AlinaHermanovich <box.lid102@gmail.com>
2026-08-31 18:16:22 +0300

    Update Narrator.tsx

--- src/components/Narrator.tsx
+++ src/components/Narrator.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { motion } from "motion/react";
 import { projects } from "@/lib/content";
