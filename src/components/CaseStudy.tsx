79250de946481909acfa1901f2f634da16e514b5
AlinaHermanovich <box.lid102@gmail.com>
2026-09-08 19:17:20 +0300

    Update CaseStudy.tsx

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -186,7 +186,7 @@ export default function CaseStudy({
                  ratio={
                         s.id === "print" && (i === 0 || i === 2)
                           ? "h-[515px]"
-                          : s.id === "print" && i >= 2
+                           s.id === "print" && (i === 0 || i === 2)
                             ? "aspect-[9/16] max-h-[420px] mx-auto"
                             : s.id === "logo"
                               ? "aspect-[4/3]"
